d0c9e319828f5edc240d0b50582139893bd97a58
kabylakee <vctrcute@gmail.com>
2023-03-05 13:20:00 +0300

    add common page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from "@angular/material/button";
+import { CommonPageComponent } from './common-page/common-page.component';
+import { GameCardComponent } from './shared/shared-components/game-card/game-card.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonPageComponent,
+    GameCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/entities/mocks/games.mock.ts
@@ -0,0 +1,60 @@
+import {IGame} from "../interfaces/game.interface";
+
+export const GAMES_MOCKS: IGame[] = [
+  {
+    title: "Last of us",
+    genre: "arcade",
+    img: "https://3dnews.ru/assets/external/illustrations/2022/12/09/1078609/0.jpg",
+    content: "It's popular relay game",
+    price: 200,
+    favourite: true,
+    sold: true,
+  },
+  {
+    title: "STEELRISING - BASTILLE EDITION",
+    genre: "arcade",
+    img: "https://image.api.playstation.com/vulcan/ap/rnd/202209/0215/XQOSn57bi1MAeId4vbMpYPcB.jpg",
+    content: "It's popular relay game",
+    price: 200,
+    favourite: true,
+    sold: false,
+  },
+  {
+    title: "MARVEL’S SPIDER-MAN: MILES MORALES",
+    genre: "arcade",
+    img: "https://image.api.playstation.com/vulcan/ap/rnd/202008/1020/bzmUpd4O5o7GtuqKE1kCDFse.jpg",
+    content: "It's popular relay game",
+    price: 200,
+    favourite: true,
+    sold: false,
+  },
+  {
+    title: "Park Beyond",
+    genre: "arcade",
+    img: "https://static.bandainamcoent.eu/high/park-beyond/01-news/park-beyond-announcement.jpg",
+    content: "It's popular relay game",
+    price: 200,
+    favourite: true,
+    sold: true,
+  },
+  {
+    title: "RATCHET & CLANK: RIFT APART",
+    genre: "arcade",
+    img: "https://insomniac.games/wp-content/uploads/2020/10/RC_RiftApart_Who_PS5-scaled.jpg",
+    content: "Скачите по измерениям вместе с Рэтчетом и Кланком, которые вступают в борьбу со злым императором из иной реальности." +
+      "Прыгайте между мирами с головокружительной скоростью вместе с межгалактическими искателями приключенийю" +
+      "Вас ждут ошеломляющая графика и сумасшедший арсенал!",
+    price: 300,
+    favourite: true,
+    sold: true,
+  },
+  {
+    title: "EXOPRIMAL DELUXE EDITION",
+    genre: "arcade",
+    img: "https://cdn.akamai.steamstatic.com/steam/subs/797202/header.jpg?t=1685992569",
+    content: "Exoprimal — это многопользовательская командная игра в стиле экшн, в которой игрокам предстоит бросить вызов самым свирепым зверям",
+    price: 240,
+    favourite: true,
+    sold: false,
+  }
+];
